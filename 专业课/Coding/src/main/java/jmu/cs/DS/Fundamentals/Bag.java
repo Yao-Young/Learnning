f96c992d6dcfc0e0684fa75a05a4aa470c4c285f
yzy <2776354449@qq.com>
2022-06-14 16:38:07 +0800

    完善了bag代码

--- 专业课/Coding/src/main/java/jmu/cs/DS/Fundamentals/Bag.java
+++ 专业课/Coding/src/main/java/jmu/cs/DS/Fundamentals/Bag.java
@@ -1,12 +1,68 @@
 package jmu.cs.DS.Fundamentals;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Bag<T> implements Iterable<T> {
 
+    public Node<T> first;
+    public int n;
+
+    public static class Node<T> {
+        public T item;
+        public Node<T> next;
+    }
+
+    public Bag() {
+        first = null;
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void add(T item) {
+        Node<T> oldFirst = first;
+        first = new Node<>();
+        first.item = item;
+        first.next = oldFirst;
+        ++n;
+    }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new LinkedIterator(first);
+    }
+
+    private class LinkedIterator implements Iterator<T> {
+        private Node<T> current;
+
+        public LinkedIterator(Node<T> first) {
+            current = first;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current!= null;
+        }
+
+        @Override
+        public T next() {
+            if(!hasNext()) throw new NoSuchElementException();
+            T item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
     }
 }
+//用到泛型，自动装箱，迭代器模式
--- 专业课/Coding/src/test/java/test.java
+++ 专业课/Coding/src/test/java/test.java
@@ -1,11 +1,21 @@
-import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.StdOut;
+import jmu.cs.DS.Fundamentals.Bag;
+
+import java.util.Iterator;
 
 public class test {
 
     public static void main(String[] args) {
 
+        Bag<String> bag = new Bag<>();
+        bag.add("yzy");
+        bag.add("zzz");
+        StdOut.println("size = " + bag.size());
+
+        Iterator it = bag.iterator();
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
 
     }
-
 }
